6f5a5a97773c970fe9b286ee095c1f11d9f982ce
ADMIN <ADMIN@DESKTOP-EJHAJJ4.mshome.net>
2025-09-23 14:51:12 +0530

    Added New POM Class for Leads Module

---
+++ ComcastCRMGUFramework2/src/main/java/com/comcast/crm/objectrepositoryutility/LeadsPage.java
@@ -0,0 +1,9 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LeadsPage {
+	@FindBy(xpath = "//img[@alt=\"Create Lead...\"]")
+	private WebElement addNewLeadBtn;
+}
