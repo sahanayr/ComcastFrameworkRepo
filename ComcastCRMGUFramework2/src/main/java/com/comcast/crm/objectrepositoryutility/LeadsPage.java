509a77221fc9c5a595c3b00aad466fe32dde800d
ADMIN <ADMIN@DESKTOP-EJHAJJ4.mshome.net>
2025-09-25 13:26:35 +0530

    Leads pom class added new element3

--- ComcastCRMGUFramework2/src/main/java/com/comcast/crm/objectrepositoryutility/LeadsPage.java
+++ ComcastCRMGUFramework2/src/main/java/com/comcast/crm/objectrepositoryutility/LeadsPage.java
@@ -6,4 +6,8 @@ import org.openqa.selenium.support.FindBy;
 public class LeadsPage {
 	@FindBy(xpath = "//img[@alt=\"Create Lead...\"]")
 	private WebElement addNewLeadBtn;
+	
+	@FindBy(name="searchBtn")
+	private WebElement ele3;
+	
 }
